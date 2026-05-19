0ee583ae155b59380add7982a9fe835c11af5d18
vinitoffiicial <vinitoffiicial@gmail.com>
2026-05-19 17:44:44 +0530

    Update app.py

--- Task4_Simple Chatbot/app.py
+++ Task4_Simple Chatbot/app.py
@@ -1,1 +1,24 @@
+def chatbot():
+    print("🤖 Chatbot: Hi! Type 'bye' to exit.")
 
+    while True:
+        user_input = input("You: ").lower()
+
+        # Remove punctuation
+        user_input = user_input.replace("!", "").replace("?", "").replace(".", "")
+
+        if "hello" in user_input or "hi" in user_input:
+            print("🤖 Chatbot: Hi!")
+
+        elif "how are you" in user_input:
+            print("🤖 Chatbot: I'm fine, thanks!")
+
+        elif "bye" in user_input:
+            print("🤖 Chatbot: Goodbye!")
+            break
+
+        else:
+            print("🤖 Chatbot: Sorry, I don't understand that.")
+
+
+chatbot()
